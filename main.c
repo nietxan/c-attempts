2c22f0f54065284f66b06944dcc6c6622df8250e
Niyetkhan <niethan.2004@gmail.com>
2024-04-14 11:55:40 +0500

    trying sockets

--- main.c
+++ main.c
@@ -1,17 +1,60 @@
-#include "linkedlist.c"
+#include <netinet/in.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#define PORT 8080
 
-#define SIZE 15
 
 int main()
 {
-	node* head = malloc(sizeof(node));
-	fill_list(head, SIZE);
-	print_list(head);
+	int opt = 1;
+	ssize_t valread;
+	struct sockaddr_in address;
+	char buffer[1024] = {0};
+	int serverfd, newsocket;
+	char* hello = "HELLO FROM SERVER";
+	socklen_t addrlen = sizeof(address);
 
-	node* reversed = malloc(sizeof(node));
-	reverse(head, SIZE);
-	print_list(reversed);
 
+	if ((serverfd = socket(AF_LOCAL, SOCK_STREAM, 0)) < 0) {
+		perror("socket");
+		exit(EXIT_FAILURE);
+	}
+
+	if (setsockopt(serverfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+		perror("setsockopt");
+		exit(EXIT_FAILURE);
+	}
+
+	address.sin_family = AF_LOCAL;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons(PORT);
+
+	if (bind(serverfd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+		perror("bind");
+		exit(EXIT_FAILURE);
+	}
+
+	if (listen(serverfd, 3) < 0) {
+		perror("accept");
+		exit(EXIT_FAILURE);
+	}
+
+	if ((newsocket = accept(serverfd, (struct sockaddr*)&address, &addrlen)) < 0) {
+		perror("accept");
+		exit(EXIT_FAILURE);
+	}	
+
+	valread = read(serverfd, buffer, 1024 - 1);
+
+	printf("%s\n", buffer);
+    send(newsocket, hello, strlen(hello), 0);
+
+	close(newsocket);
+	close(serverfd);
 	return 0;
 }
+
